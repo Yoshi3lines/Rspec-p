944331b983c910dc3068d37f83ade6df7080c814
Yoshi_3line <ny3line@gmail.com>
2022-04-02 08:37:31 +0900

    test_9session終了

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,51 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  it "ユーザー単位では重複したプロジェクト名を許可しない" do
-    user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "joetester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
+  it { is_expected.to validate_uniqueness_of(:name).scoped_to(:user_id) }
 
-    user.projects.create(
-      name: "Test Project",
-    )
+  # ここの部分だけはコメントアウト
+  # it "ユーザー単位では重複したプロジェクト名を許可しない" do
+  #   user = User.create(
+  #     first_name: "Joe",
+  #     last_name: "Tester",
+  #     email: "joetester@example.com",
+  #     password: "dottle-nouveau-pavilion-tights-furze",
+  #   )
+
+  #   user.projects.create(
+  #     name: "Test Project",
+  #   )
     
-    new_project = user.projects.build(
-      name: "Test Project",
-    )
+  #   new_project = user.projects.build(
+  #     name: "Test Project",
+  #   )
 
-    new_project.valid?
-    expect(new_project.errors[:name]).to include("has already been taken")
-  end
+  #   new_project.valid?
+  #   expect(new_project.errors[:name]).to include("has already been taken")
+  # end
 
-  it "二人のユーザーが同じ名前を使うことは許可する" do
-    user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "joetester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
+  # it "二人のユーザーが同じ名前を使うことは許可する" do
+  #   user = User.create(
+  #     first_name: "Joe",
+  #     last_name: "Tester",
+  #     email: "joetester@example.com",
+  #     password: "dottle-nouveau-pavilion-tights-furze",
+  #   )
     
-    user.projects.create(
-      name: "Test Project",
-    )
+  #   user.projects.create(
+  #     name: "Test Project",
+  #   )
 
-    other_user = User.create(
-      first_name: "Jane",
-      last_name: "Tester",
-      email: "janetester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
+  #   other_user = User.create(
+  #     first_name: "Jane",
+  #     last_name: "Tester",
+  #     email: "janetester@example.com",
+  #     password: "dottle-nouveau-pavilion-tights-furze",
+  #   )
 
-    other_project = other_user.projects.build(
-      name: "Test Project",
-    )
+  #   other_project = other_user.projects.build(
+  #     name: "Test Project",
+  #   )
 
-    expect(other_project).to be_valid
-  end
+  #   expect(other_project).to be_valid
+  # end
 end
 
 describe "遅延ステータス" do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,30 +17,10 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
 
-  it "名がなければ無効な状態であること" do
-    user = FactoryBot.build(:user, first_name: nil)
-    user.valid?
-    expect(user.errors[:first_name]).to include("can't be blank")
-  end
-
-  it "姓がなければ無効な状態であること" do
-    user = FactoryBot.build(:user, last_name: nil)
-    user.valid?
-    expect(user.errors[:last_name]).to include("can't be blank")
-  end
-
-  it "メールアドレスがなければ無効な状態であること" do
-    user = FactoryBot.build(:user, email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
-  end
-
-  it "重複したメールアドレスなら無効な状態であること" do
-    FactoryBot.create(:user, email: "aaron@example.com")
-    user = FactoryBot.build(:user, email: "aaron@example.com")
-    user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
-  end
+  it { is_expected.to validate_presence_of :first_name }
+  it { is_expected.to validate_presence_of :last_name }
+  it { is_expected.to validate_presence_of :email }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
 
   it "ユーザーのフルネームを文字列として返すこと" do
     user = FactoryBot.build(:user, first_name: "John", last_name: "Doe")
@@ -53,3 +33,52 @@ RSpec.describe User, type: :model do
     expect(:true).to be_truthy
   end
 end
+
+
+# RSpec.describe User, type: :model do 変更前p156
+#   it "姓、名、メール、パスワードがあれば有効な状態であること" do
+#     user = User.new(
+#       first_name: "Aaron",
+#       last_name: "Summer",
+#       email: "tester@example.com",
+#       password: "dottle-nouveau-pavilion-tights-furze",
+#     )
+#     expect(user).to be_valid
+#   end
+
+#   it "名がなければ無効な状態であること" do
+#     user = FactoryBot.build(:user, first_name: nil)
+#     user.valid?
+#     expect(user.errors[:first_name]).to include("can't be blank")
+#   end
+
+#   it "姓がなければ無効な状態であること" do
+#     user = FactoryBot.build(:user, last_name: nil)
+#     user.valid?
+#     expect(user.errors[:last_name]).to include("can't be blank")
+#   end
+
+#   it "メールアドレスがなければ無効な状態であること" do
+#     user = FactoryBot.build(:user, email: nil)
+#     user.valid?
+#     expect(user.errors[:email]).to include("can't be blank")
+#   end
+
+#   it "重複したメールアドレスなら無効な状態であること" do
+#     FactoryBot.create(:user, email: "aaron@example.com")
+#     user = FactoryBot.build(:user, email: "aaron@example.com")
+#     user.valid?
+#     expect(user.errors[:email]).to include("has already been taken")
+#   end
+
+#   it "ユーザーのフルネームを文字列として返すこと" do
+#     user = FactoryBot.build(:user, first_name: "John", last_name: "Doe")
+#     expect(user.name).to eq "John Doe"
+#   end
+
+#   it "複数のユーザーで同じアドレスを登録してみる（シーケンスをテスト）" do
+#     user1 = FactoryBot.create(:user)
+#     user2 = FactoryBot.create(:user)
+#     expect(:true).to be_truthy
+#   end
+# end
